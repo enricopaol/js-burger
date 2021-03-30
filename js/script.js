8ba26919d2c2df402649e22ba16abf21f1b53414
Enrico Paolazzi <paolazzienrico@gmail.com>
2021-03-30 15:17:03 +0200

    First Part - Name of Burger

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var calculatePriceBurger = document.getElementById('button-price-burger');
+calculatePriceBurger.addEventListener('click', function() {
+
+    var nomeBurger = document.getElementById('input-name-burger');
+    console.log(nomeBurger.value);
+
+});
+
+
